f1c2a45d3031ada9ae96a4670837a3e7b196f589
Coder-Jon014 <jon.coombs14@gmail.com>
2024-08-06 23:06:06 -0500

    Made Banner Responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Banner: React.FC = () => {
   return (
-    <div className="bg-peniel-secondary hover:bg-peniel-main transition-colors duration-300 text-white p-4 text-center font-bold">
-      Approved by The Institute of Leadership and Management (UK)
+    <div className="bg-peniel-secondary hover:bg-peniel-main transition-colors duration-300 text-white p-2 sm:p-3 md:p-4 text-center">
+      <p className="text-xs sm:text-sm md:text-base lg:text-lg font-bold">
+        Approved by The Institute of Leadership and Management (UK)
+      </p>
     </div>
   );
 };
