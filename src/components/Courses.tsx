4d3ed360d0d4fff89f9c38f1a21fa5bf8ad9a468
Coder-Jon014 <jon.coombs14@gmail.com>
2024-08-06 22:25:55 -0500

    Adjusted Courses

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 
 const courses = [
-  { title: 'Leadership and Management', description: 'Customized courses to develop effective leaders and managers.' },
-  { title: 'Customer Service', description: 'A 16-week course to master the art of delivering world-class customer service.' },
-  { title: 'Business Administration', description: 'Develop industry skills and knowledge to steer your company towards success.' },
+  { title: 'Leadership and Management', description: 'The Leadership and Management Course is provided for adult learners as a customized course designed to meet the training needs of the individual learner regardless of socio-economic status who wants to become an effective leader or manager of his or her business; perform optimally in an organization.' },
+  { title: 'Customer Service', description: 'The Customer Service course is a 16-week course for business/organization leaders, managers, entrepreneurs and workers to begin an in-depth journey into what is involved in delivering quality customer service at world-class levels to both internal and external customers.' },
+  { title: 'Business Administration', description: 'The Business Administration course is to guide adult learners through the process of developing their industry skills and knowledge in order for them to help in steering their companies towards its goals and compete with other companies.' },
 ];
 
 const Courses = () => {
@@ -12,24 +12,22 @@ const Courses = () => {
       <div className="absolute -left-10 top-1/2 transform -translate-y-1/2 bg-peniel-main text-white py-2 px-4 rotate-90 origin-top-left">
         Courses
       </div>
-      <div className="grid grid-cols-3 grid-rows-5 gap-y-6">
+      <div className="grid grid-cols-3 grid-rows-4">
         <div className="col-span-2 row-start-1">
           <h2 className="text-4xl font-bold mb-4">Courses We Offer</h2>
           <p className="text-gray-400">Designed to meet the training needs of leaders-to-be. Here are some of the courses we offer</p>
         </div>
-        <div className="col-span-2 row-start-2">
-          <p className="text-gray-400"> </p>
-        </div>
+
         {courses.map((course, index) => (
-          <div key={index} className={`col-span-2 row-start-${index + 3} flex`}>
-            <div className="text-peniel-secondary font-bold text-xl mr-4">{(index + 1).toString().padStart(2, '0')}</div>
+          <div key={index} className={`col-span-2 row-start-${index + 2} flex mr-20`}>
+            <div className="text-[#DC241C] font-bold text-xl mr-4">{(index + 1).toString().padStart(2, '0')}</div>
             <div>
               <h3 className="text-xl font-bold mb-2">{course.title}</h3>
               <p className="text-gray-400">{course.description}</p>
             </div>
           </div>
         ))}
-        <div className="row-span-3 row-start-3 col-start-3">
+        <div className="row-span-3 row-start-2 col-start-3">
           <Image 
             src="https://utfs.io/f/8110362e-09f2-4df4-a857-81057b25ec4e-3h50mg.jpg"
             alt="Courses Image"
