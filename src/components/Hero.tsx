51793d2e87c81161c23436a3f8614ea4ebd378bf
Coder-Jon014 <jon.coombs14@gmail.com>
2024-08-09 19:29:42 -0500

    Updated Gradient Used For Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link';
 const Hero = () => {
   return (
     <div className="flex flex-col md:flex-row justify-between items-center py-8 md:py-16 px-4 md:px-8 bg-white">
-      <div className="w-full md:w-1/2 px-4 md:px-20 mb-8 md:mb-0 text-center md:text-left">
-        <h1 className="text-3xl md:text-4xl font-bold mb-4">
+      <div className="w-full md:w-3/4 px-4 md:px-20 mb-8 md:mb-0 text-center md:text-left">
+        <h1 className="text-3xl md:text-6xl font-bold mb-4">
           Empowering Your Future with{' '}
           <span className="relative inline-block">
-            <span className="absolute inset-x-0 bottom-0 h-3 bg-peniel-main opacity-50"></span>
+            <span className="absolute inset-x-0 bottom-0 h-3 bg-gradient-to-r from-indigo-300 to-purple-300 dark:from-indigo-500 dark:to-purple-500"></span>
             <span className="relative z-10">Quality Education</span>
           </span>
         </h1>
-        <p className="text-sm md:text-base text-gray-500 mb-6">Affordable, Flexible, and Accessible Training Programs for Aspiring Leaders and Professionals.</p>
+        <p className="text-sm md:text-base md:w-1/2 text-gray-500 mb-6">Affordable, Flexible, and Accessible Training Programs for Aspiring Leaders and Professionals.</p>
         <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4">
           <Button className="w-full sm:w-auto bg-[#4B0082] hover:bg-[#3D0069] text-white flex items-center justify-center gap-2">
             Join Us
