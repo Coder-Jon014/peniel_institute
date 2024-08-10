efb4c236d6b7872081e31cf22c3b2a10762cddab
Coder-Jon014 <jon.coombs14@gmail.com>
2024-08-09 19:29:24 -0500

    Reduced Size of Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,14 +16,14 @@ const Navbar = () => {
           <Image
             src="https://utfs.io/f/59601084-3869-4731-8516-4f4b56b4fb2d-du3l9k.svg"
             alt="Peniel Logo"
-            width={200}
+            width={150}
             height={15}
             priority
           />
         </div>
         <div className="hidden md:flex space-x-6">
           <Link href="/" className="hover:text-[#DC241C]">Home</Link>
-          <Link href="#courses" className="hover:text-[#DC241C]">Courses</Link>
+          <Link href="/#courses" className="hover:text-[#DC241C]">Courses</Link>
           <Link href="/about" className="hover:text-[#DC241C]">About Us</Link>
         </div>
         <Button className="hidden md:block hover:bg-[#DC241C] bg-[#2C1474]">Get Started</Button>
